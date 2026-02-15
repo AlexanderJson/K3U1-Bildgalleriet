ef3cf85d8bfae4c53fe02f01c1e2a86b7b73e448
alexanderjson <alexanderjson@hotmail.com>
2026-02-15 04:35:58 +0100

    added images to data

---
+++ comps/imageFrame.js
@@ -0,0 +1,8 @@
+export const imageFrame = (url) =>
+{
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    productImage.src = url;
+    div.append(img);
+
+}
